813f2717379496ce606ea9cc71dcaa4866182cbf
Breno Santin <brenolimaa96@gmail.com>
2026-02-08 22:03:53 +0100

    created migration

---
+++ Migrations/20260208210150_MakeDeliveryDateAndPeriodNullable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarmitaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeDeliveryDateAndPeriodNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DeliveryPeriod",
+                table: "DeliveryInfo",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "DeliveryInfo",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DeliveryInfo",
+                keyColumn: "DeliveryPeriod",
+                keyValue: null,
+                column: "DeliveryPeriod",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeliveryPeriod",
+                table: "DeliveryInfo",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "DeliveryInfo",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+        }
+    }
+}
